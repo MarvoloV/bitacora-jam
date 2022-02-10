72c0431608c1ef862e484bbce4f7ebd2b73d895d
Jorge Luis Adco <jorgead0812@gmail.com>
2022-02-10 13:59:56 -0500

    creacion de Vista Error 404

---
+++ src/Pages/DashboardPage/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Sidebar from '../../Components/Sidebar';
+// import Login from '../../Components/Login/index';
+
+// eslint-disable-next-line react/no-children-prop
+const DashboardPage = () => <Sidebar />;
+
+export default DashboardPage;
---
+++ src/Pages/Error404/index.jsx
@@ -0,0 +1,41 @@
+import { Box, Heading, Text, Button, Center } from '@chakra-ui/react';
+
+const NotFound = () => (
+  <Box
+    textAlign="center"
+    py={10}
+    px={6}
+    height="100vh"
+    display="flex"
+    alignItems="center"
+    justifyContent="center"
+  >
+    <Center flexDirection="column" width="100vW" height="50vh">
+      <Heading
+        display="inline-block"
+        as="h1"
+        size="4xl"
+        bgGradient="linear(to-r, teal.400, teal.600)"
+        backgroundClip="text"
+      >
+        404
+      </Heading>
+      <Text fontSize="30px" mt={3} mb={2}>
+        Página no encontrada
+      </Text>
+      <Text color="gray.500" mb={6} fontSize={20}>
+        La página que busca no existe
+      </Text>
+
+      <Button
+        colorScheme="teal"
+        bgGradient="linear(to-r, teal.400, teal.500, teal.600)"
+        color="white"
+        variant="solid"
+      >
+        Go to Home
+      </Button>
+    </Center>
+  </Box>
+);
+export default NotFound;
